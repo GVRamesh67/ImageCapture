897ae63cbb1aa75f112a0b7cfb15c7632c782681
GVRamesh67 <gvramesh67@gmail.com>
2017-01-05 11:27:14 +0530

    Add files via upload

--- js/Tovpsofficialvisitpass.js
+++ js/Tovpsofficialvisitpass.js
@@ -1,15 +1,38 @@
 $('#idAdmin').addClass('display-none');
+//From Template Created by GV Ramesh :-)
 var divUserAuthenticated;
+var visitorPhoneNo;
+var visitorOTP;
 var divWhereVisit;
 var errorMessage;
-//From Template Created by GV Ramesh :-)
+
+var visitorCredentials = function (visitorPhoneNo_, visitorActualOTP_) {
+    this.visitorPhoneNo = visitorPhoneNo_;
+    this.visitorActualOTP = visitorActualOTP_;
+    this.visitorEnteredOTP = '';
+    this.divUserAuthenticated = false;
+    this.setEnteredOTP = function (visitorEnteredOTP_) {
+        this.visitorEnteredOTP = visitorEnteredOTP_;
+    };
+    this.authenticate = function () {
+        if (this.visitorActualOTP === this.visitorEnteredOTP) {
+            this.divUserAuthenticated = true;
+        } else {
+            this.divUserAuthenticated = false;
+        }
+        divUserAuthenticated(this.divUserAuthenticated);
+    };
+};
+var koVisitorCredentials;
 var ViewModal = function () {
     self = this;
     //Initializing all the KO Variables (The ones that are bound to DOM
     //Elements)
     divUserAuthenticated = ko.observable(false);
+    koVisitorCredentials = ko.observable();
     divWhereVisit = ko.observable('P');
     errorMessage = ko.observable('');
+    koVisitorCredentials(new visitorCredentials('9989121356', '123'));
 };
 ko.applyBindings(new ViewModal());
 function processSave() {
